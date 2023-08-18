b7e98ee0b46a34d5d7e443b5399301b8d37dac87
Federico <fedeale2000@gmail.com>
2023-08-18 13:26:15 -0300

    fix responsive

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const HomePage = async () => {
 
   return (
     <section className="mx-auto p-5">
-      <div className="grid grid-cols-4 place-content-center gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-content-center gap-5">
         {users.data.map((user) => (
           <Link key={user.id} href={`/users/${user.id}`}>
             <UserCard user={user} />
